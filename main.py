2912f5bb5cda198a39616e3437eb4641330cbfed
sribot21 <92053681+sribot21@users.noreply.github.com>
2021-10-07 01:56:45 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import os
 import telebot
 
 
-bot = telebot.TeleBot("API එක දාහන් උස්සන්නෙ නැතුව මගෙ ඒව")
+bot = telebot.Telebot("2097395997:AAGieE29KKlcAWKoq2It2KoG1LsVaGPj10o")
 
 
 @bot.message_handler(commands=["start"])
 def send_welcome(message):
-  bot.reply_to(message, "Hello! I'm Uvindu Bro Chat Bot")
+bot.reply_to(message, "Hello! I'm ad token Chat Bot")
 
 
 @bot.message_handler(commands=["how"])
-def send_message(message):
-  bot.send_message(message.chat.id, "https://youtube.com/c/Uvindubro")
+def send_message(message)
+bot.send_message(message, "https://www.youtube.com/channel/UCasEPrsQWCgrTl8g6OpAuJw")
 
 
+bot.polling() 
 
-bot.polling()
